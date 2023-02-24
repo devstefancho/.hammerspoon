1b835c6d8bdfb12bb14ef524c05d2b5d41fd9503
devstefancho <skku7714@gmail.com>
2023-02-25 00:02:53 +0900

    Add Remapper

--- init.lua
+++ init.lua
@@ -1,5 +1,13 @@
 local fn = hs.loadSpoon("devstefancho")
 
+-- [[ Launch App ]]
 fn.hotkeyBindApp({ "alt" }, "1", "iTerm")
 fn.hotkeyBindApp({ "alt" }, "2", "Google Chrome")
 fn.hotkeyBindApp({ "alt" }, "3", "Obsidian")
+
+-- [[ Remapper ]]
+local FRemap = hs.loadSpoon("foundation_remapping")
+
+local remapper = FRemap.new()
+remapper:remap("rcmd", "F19") -- 한/영 변환키
+remapper:register()
